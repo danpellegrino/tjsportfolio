61be475a4f7c2cd1cbbf5d29468db7dbb76ef42d
Daniel Pellegrino <danpellegrino1@gmail.com>
2024-02-28 11:38:35 -0500

    Update CTA text

--- components/Cta.jsx
+++ components/Cta.jsx
@@ -7,7 +7,7 @@ const Cta = () => {
       <div className="container mx-auto">
         <div className='flex flex-col items-center'>
             <h2 className='h2 max-w-xl text-center mb-8'>
-                Prepared to turn your ideas into reality? I'm here to help
+                Lorem ipsum (CTA text here)
             </h2>
             <Link href='/contact'>
             <Button className="w-[165px]">Contact me</Button>
