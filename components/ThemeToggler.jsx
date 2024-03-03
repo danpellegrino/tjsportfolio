9f94f9d4092602aba104401dc4c6c4f96e3ea2fa
Daniel Pellegrino <64383789+danpellegrino@users.noreply.github.com>
2024-03-02 23:36:44 -0500

    Update ThemeToggler icons

--- components/ThemeToggler.jsx
+++ components/ThemeToggler.jsx
@@ -11,10 +11,10 @@ const ThemeToggler = () => {
       <Button
         variant='outline'
         size='icon'
-        onClick={()=> setTheme(theme === 'dark' ? 'light' : 'dark')}
+        onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
       >
-        <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-        <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+        <SunIcon className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 text-black' />
+        <MoonIcon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white' />
       </Button>
     </div>
   );
