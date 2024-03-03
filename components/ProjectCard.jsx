8602119a6e1323dbba4c039b321924aa26a3f5fd
Daniel Pellegrino <64383789+danpellegrino@users.noreply.github.com>
2024-03-03 05:25:40 -0500

    Refactor ProjectCard component to support image and video display

--- components/ProjectCard.jsx
+++ components/ProjectCard.jsx
@@ -11,31 +11,51 @@ const ProjectCard = ({ project }) => {
         <Card className='group overflow-hidden relative'>
             <CardHeader className='p-0'>
                 {/* image */}
-                <div className='relative w-full h-[300px] flex items-center justify-center bg-primary/15 dark:bg-secondary/20 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden'>
-                    <Image
-                        className='absolute bottom-0 shadow-2xl'
-                        src={project.image}
-                        width={247}
-                        height={250}
-                        alt=''
-                        priority
-                    />
-                    {/* btn links */}
-                    <div className='flex gap-x-4'>
-                        <Link
-                            href={project.link}
-                            className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200'
-                        >
-                            <Link2Icon className='text-white' />
-                        </Link>
-                        <Link
-                            href={project.drive}
-                            className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200'
-                        >
-                            <FontAwesomeIcon icon={faGoogleDrive} className='text-white' />
-                        </Link>
+                {project.image &&
+                    <div className='relative w-full h-[300px] flex items-center justify-center bg-primary/15 dark:bg-secondary/20 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden'>
+                        {/* image */}
+                        <Image
+                            className={`absolute drop-shadow-2xl`}
+                            src={project.image}
+                            width={project.width || project.height || 247}
+                            height={project.height || project.width || 250}
+                            alt=''
+                            priority
+                        />
+
+                        {/* btn links */}
+                        <div className='flex gap-x-4'>
+                            <Link
+                                href={project.link}
+                                className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200'
+                            >
+                                <Link2Icon className='text-white' />
+                            </Link>
+                            <Link
+                                href={project.drive}
+                                className='bg-secondary w-[54px] h-[54px] rounded-full flex justify-center items-center scale-0 opacity-0 group-hover:scale-100 group-hover:opacity-100 transition-all duration-200'
+                            >
+                                <FontAwesomeIcon icon={faGoogleDrive} className='text-white' />
+                            </Link>
+                        </div>
                     </div>
-                </div>
+                }
+                {/* video */}
+                {project.video &&
+                    <div className='relative w-full h-[300px] flex items-center justify-center bg-primary/15 dark:bg-secondary/20 xl:bg-work_project_bg_light xl:dark:bg-work_project_bg_dark xl:bg-[110%] xl:bg-no-repeat overflow-hidden'>
+                        {/* image */}
+                        <video
+                            className={`absolute shadow-2xl`}
+                            width={project.width || project.height || 247}
+                            height={project.height || project.width || 250}
+                            controls
+                            preload='metadata'
+                        >
+                            <source src={project.video} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                    </div>
+                }
             </CardHeader>
             <div className='h-full px-8 py-6'>
                 <Badge className='uppercase text-sm font-medium mb-2 absolute top-4 left-5'>
