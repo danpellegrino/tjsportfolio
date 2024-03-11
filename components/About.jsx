0916c9057dd25bdc6dc603e5b9f1f7c861607657
Daniel Pellegrino <danpellegrino1@gmail.com>
2024-03-11 01:36:14 -0400

    Update born date

--- components/About.jsx
+++ components/About.jsx
@@ -1,6 +1,6 @@
-import DevImg from './DevImg';
-import Image from 'next/image';
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import DevImg from "./DevImg";
+import Image from "next/image";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 import {
   User2,
@@ -9,109 +9,109 @@ import {
   PhoneCall,
   GraduationCap,
   Calendar,
-  Briefcase
-} from 'lucide-react';
+  Briefcase,
+} from "lucide-react";
 
 const infoData = [
   {
     icon: <User2 size={20} />,
-    text: 'Tyler Senecal',
+    text: "Tyler Senecal",
   },
   {
     icon: <PhoneCall size={20} />,
-    text: '+518 299 8486',
+    text: "+518 299 8486",
   },
   {
     icon: <MailIcon size={20} />,
-    text: 'contact@tjs-designs.com',
+    text: "contact@tjs-designs.com",
   },
   {
     icon: <Calendar size={20} />,
-    text: 'Born on 23 Jun, 2002',
+    text: "Born on June 23, 2002",
   },
   {
     icon: <GraduationCap size={20} />,
-    text: 'Bachelor in Communications and Media Design',
+    text: "Bachelor in Communications and Media Design",
   },
   {
     icon: <HomeIcon size={20} />,
-    text: '101 Lowell Avenue Utica, NY, USA',
+    text: "101 Lowell Avenue Utica, NY, USA",
   },
 ];
 
 const qualificationData = [
   {
-    title: 'education',
+    title: "education",
     data: [
       {
-        university: 'SUNY Polytechnic Institute',
-        qualification: 'Bachelor of Science',
-        years: '2020 - 2024',
+        university: "SUNY Polytechnic Institute",
+        qualification: "Bachelor of Science",
+        years: "2020 - 2024",
       },
       {
-        university: 'SUNY Polytechnic Institute',
-        qualification: 'Master of Science',
-        years: '2024 - 2025',
+        university: "SUNY Polytechnic Institute",
+        qualification: "Master of Science",
+        years: "2024 - 2025",
       },
-    ]
+    ],
   },
   {
-    title: 'experience',
+    title: "experience",
     data: [
       {
-        company: 'Student Government at Utica',
-        role: 'Director of Public Relations',
-        years: '2021 - 2022',
+        company: "Student Government at Utica",
+        role: "Director of Public Relations",
+        years: "2021 - 2022",
       },
       {
-        company: 'Student Government at Utica',
-        role: 'Creative Design Coordinator',
-        years: '2022 - Present',
+        company: "Student Government at Utica",
+        role: "Creative Design Coordinator",
+        years: "2022 - Present",
       },
-    ]
+    ],
   },
 ];
 
 const skillData = [
   {
-    title: 'skills',
+    title: "skills",
     data: [
       {
-        name: 'UI/UX Design',
+        name: "UI/UX Design",
       },
       {
-        name: 'Advertising',
+        name: "Advertising",
       },
       {
-        name: 'Social Media Marketing',
+        name: "Social Media Marketing",
       },
       {
-        name: 'HTML, CSS',
+        name: "HTML, CSS",
       },
-    ]
+    ],
   },
   {
-    title: 'tools',
+    title: "tools",
     data: [
       {
-        imgPath: '/about/photoshop.svg',
+        imgPath: "/about/photoshop.svg",
       },
       {
-        imgPath: '/about/illustrator.svg',
+        imgPath: "/about/illustrator.svg",
       },
       {
-        imgPath: '/about/aftereffects.svg',
+        imgPath: "/about/aftereffects.svg",
       },
       {
-        imgPath: '/about/indesign.svg',
+        imgPath: "/about/indesign.svg",
       },
       {
-        imgPath: '/about/premierepro.svg',
+        imgPath: "/about/premierepro.svg",
       },
       {
-        imgPath: '/about/canva.svg',
+        imgPath: "/about/canva.svg",
       },
-    ]
+    ],
   },
 ];
 
@@ -121,47 +121,56 @@ const About = () => {
   };
 
   return (
-    <section className='xl:h-[860px] pb-12 md:py-12 lg:py-36'>
-      <div className='container mx-auto'>
-        <h2 className='section-title mb-8 xl:mb-16 text-center mx-auto'>
+    <section className="xl:h-[860px] pb-12 md:py-12 lg:py-36">
+      <div className="container mx-auto">
+        <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
           About me
         </h2>
-        <div className='flex flex-col xl:flex-row'>
+        <div className="flex flex-col xl:flex-row">
           {/* Image */}
-          <div className='hidden xl:flex flex-1 relative'>
+          <div className="hidden xl:flex flex-1 relative">
             <DevImg
-              containerStyles='bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative'
-              imgSrc='/about/developer.png'
+              containerStyles="bg-about_shape_light dark:bg-about_shape_dark w-[505px] h-[505px] bg-no-repeat relative"
+              imgSrc="/about/developer.png"
             />
           </div>
           {/* Tabs */}
-          <div className='flex-1'>
-            <Tabs defaultValue='personal'>
-              <TabsList className='w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none'>
-                <TabsTrigger className='w-[162px] xl:w-auto' value='personal'>Personal Info</TabsTrigger>
-                <TabsTrigger className='w-[162px] xl:w-auto' value='qualifications'>Qualifications</TabsTrigger>
-                <TabsTrigger className='w-[162px] xl:w-auto' value='skills'>Skills</TabsTrigger>
+          <div className="flex-1">
+            <Tabs defaultValue="personal">
+              <TabsList className="w-full grid xl:grid-cols-3 xl:max-w-[520px] xl:border dark:border-none">
+                <TabsTrigger className="w-[162px] xl:w-auto" value="personal">
+                  Personal Info
+                </TabsTrigger>
+                <TabsTrigger
+                  className="w-[162px] xl:w-auto"
+                  value="qualifications"
+                >
+                  Qualifications
+                </TabsTrigger>
+                <TabsTrigger className="w-[162px] xl:w-auto" value="skills">
+                  Skills
+                </TabsTrigger>
               </TabsList>
               {/* Tabs Content */}
-              <div className='text-lg mt-12 xl:mt-8'>
+              <div className="text-lg mt-12 xl:mt-8">
                 {/* Personal */}
-                <TabsContent value='personal'>
-                  <div className='text-center xl:text-left'>
-                    <h3 className='h3 mb-4'>
+                <TabsContent value="personal">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-4">
                       Placeholder Title (Personal Info)
                     </h3>
-                    <p className='subtitle max-w-xl mx-auto xl:mx-0'>
+                    <p className="subtitle max-w-xl mx-auto xl:mx-0">
                       modern graphic designer
                     </p>
                     {/* Icons */}
-                    <div className='grid xl:grid-cols-2 gap-4 mb-4'>
+                    <div className="grid xl:grid-cols-2 gap-4 mb-4">
                       {infoData.map((item, index) => {
                         return (
                           <div
-                            className='flex items-center gap-x-4 mx-auto xl:mx-0'
+                            className="flex items-center gap-x-4 mx-auto xl:mx-0"
                             key={index}
                           >
-                            <div className='text-primary'>{item.icon}</div>
+                            <div className="text-primary">{item.icon}</div>
                             <div>{item.text}</div>
                           </div>
                         );
@@ -178,126 +187,124 @@ const About = () => {
                   </div>
                 </TabsContent>
                 {/* Qualifications */}
-                <TabsContent value='qualifications'>
+                <TabsContent value="qualifications">
                   <div>
-                    <h3 className='h3 mb-8 text-center xl:text-left'>
+                    <h3 className="h3 mb-8 text-center xl:text-left">
                       Placeholder Title (Qualifications)
                     </h3>
                     {/* Experience & Education Wrapper */}
-                    <div className='grid md:grid-cols-2 gap-y-8'>
+                    <div className="grid md:grid-cols-2 gap-y-8">
                       {/* Experience */}
-                      <div className='flex flex-col gap-y-6'>
-                        <div className='flex gap-x-4 items-center text-[22px] text-primary'>
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
                           <Briefcase />
-                          <h4 className='capitalize font-medium'>
-                            {getData(qualificationData, 'experience').title}
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "experience").title}
                           </h4>
                         </div>
                         {/* List */}
-                        <div className='flex flex-col gap-y-8'>
-                          {getData(qualificationData, 'experience').data.map((item, index) => {
-                            const { company, role, years } = item;
-                            return (
-                              <div
-                                className='flex gap-x-8 group'
-                                key={index}
-                              >
-                                <div className='h-[84px] w-[1px] bg-border relative ml-2'>
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "experience").data.map(
+                            (item, index) => {
+                              const { company, role, years } = item;
+                              return (
+                                <div className="flex gap-x-8 group" key={index}>
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div>
+                                      <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                    </div>
+                                  </div>
                                   <div>
-                                    <div className='w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500'></div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {company}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {role}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
                                   </div>
                                 </div>
-                                <div>
-                                  <div className='font-semibold text-xl leading-none mb-2'>
-                                    {company}
-                                  </div>
-                                  <div className='text-lg leading-none text-muted-foreground mb-4'>
-                                    {role}
-                                  </div>
-                                  <div className='text-base font-medium'>
-                                    {years}
-                                  </div>
-                                </div>
-                              </div>
-                            );
-                          })}
+                              );
+                            }
+                          )}
                         </div>
                       </div>
                       {/* Education */}
-                      <div className='flex flex-col gap-y-6'>
-                        <div className='flex gap-x-4 items-center text-[22px] text-primary'>
+                      <div className="flex flex-col gap-y-6">
+                        <div className="flex gap-x-4 items-center text-[22px] text-primary">
                           <GraduationCap size={28} />
-                          <h4 className='capitalize font-medium'>
-                            {getData(qualificationData, 'education').title}
+                          <h4 className="capitalize font-medium">
+                            {getData(qualificationData, "education").title}
                           </h4>
                         </div>
                         {/* List */}
-                        <div className='flex flex-col gap-y-8'>
-                          {getData(qualificationData, 'education').data.map((item, index) => {
-                            const { university, qualification, years } = item;
-                            return (
-                              <div
-                                className='flex gap-x-8 group'
-                                key={index}
-                              >
-                                <div className='h-[84px] w-[1px] bg-border relative ml-2'>
+                        <div className="flex flex-col gap-y-8">
+                          {getData(qualificationData, "education").data.map(
+                            (item, index) => {
+                              const { university, qualification, years } = item;
+                              return (
+                                <div className="flex gap-x-8 group" key={index}>
+                                  <div className="h-[84px] w-[1px] bg-border relative ml-2">
+                                    <div>
+                                      <div className="w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500"></div>
+                                    </div>
+                                  </div>
                                   <div>
-                                    <div className='w-[11px] h-[11px] rounded-full bg-primary absolute -left-[5px] group-hover:translate-y-[84px] transition-all duration-500'></div>
+                                    <div className="font-semibold text-xl leading-none mb-2">
+                                      {university}
+                                    </div>
+                                    <div className="text-lg leading-none text-muted-foreground mb-4">
+                                      {qualification}
+                                    </div>
+                                    <div className="text-base font-medium">
+                                      {years}
+                                    </div>
                                   </div>
                                 </div>
-                                <div>
-                                  <div className='font-semibold text-xl leading-none mb-2'>
-                                    {university}
-                                  </div>
-                                  <div className='text-lg leading-none text-muted-foreground mb-4'>
-                                    {qualification}
-                                  </div>
-                                  <div className='text-base font-medium'>
-                                    {years}
-                                  </div>
-                                </div>
-                              </div>
-                            );
-                          })}
+                              );
+                            }
+                          )}
                         </div>
                       </div>
                     </div>
                   </div>
                 </TabsContent>
                 {/* Skills */}
-                <TabsContent value='skills'>
-                  <div className='text-center xl:text-left'>
-                    <h3 className='h3 mb-8'>
-                      Placeholder Title (Skills)
-                    </h3>
+                <TabsContent value="skills">
+                  <div className="text-center xl:text-left">
+                    <h3 className="h3 mb-8">Placeholder Title (Skills)</h3>
                     {/* Skills */}
-                    <div className='mb-16'>
-                      <h4 className='text-xl font-semibold mb-2'>Skills</h4>
-                      <div className='border-b border-border mb-4'></div>
+                    <div className="mb-16">
+                      <h4 className="text-xl font-semibold mb-2">Skills</h4>
+                      <div className="border-b border-border mb-4"></div>
                       {/* Skill List */}
                       <div>
-                        {getData(skillData, 'skills').data.map((item, index) => {
-                          const { name } = item;
-                          return (
-                            <div
-                              className='w-2/4 mb-2.5 text-center xl:text-left mx-auto xl:mx-0'
-                              key={index}
-                            >
-                              <div className='font-medium'>{name}</div>
-                            </div>
-                          );
-                        })}
+                        {getData(skillData, "skills").data.map(
+                          (item, index) => {
+                            const { name } = item;
+                            return (
+                              <div
+                                className="w-2/4 mb-2.5 text-center xl:text-left mx-auto xl:mx-0"
+                                key={index}
+                              >
+                                <div className="font-medium">{name}</div>
+                              </div>
+                            );
+                          }
+                        )}
                       </div>
                     </div>
                     {/* Tools */}
                     <div>
-                      <h4 className='text-xl font-semibold mb-2 xl:text-left'>
+                      <h4 className="text-xl font-semibold mb-2 xl:text-left">
                         Tools
                       </h4>
-                      <div className='border-b border-border mb-4'></div>
+                      <div className="border-b border-border mb-4"></div>
                       {/* Tool List */}
-                      <div className='flex gap-x-8 justify-center xl:justify-start'>
-                        {getData(skillData, 'tools').data.map((item, index) => {
+                      <div className="flex gap-x-8 justify-center xl:justify-start">
+                        {getData(skillData, "tools").data.map((item, index) => {
                           const { imgPath } = item;
                           return (
                             <div key={index}>
@@ -305,7 +312,7 @@ const About = () => {
                                 src={imgPath}
                                 width={48}
                                 height={48}
-                                alt=''
+                                alt=""
                                 priority
                               />
                             </div>
@@ -322,6 +329,6 @@ const About = () => {
       </div>
     </section>
   );
-}
+};
 
-export default About
+export default About;
