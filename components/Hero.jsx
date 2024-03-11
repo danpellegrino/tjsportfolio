e1a9029976df7b8b2308344db259491cd14ff4a4
Daniel Pellegrino <danpellegrino1@gmail.com>
2024-03-11 01:08:58 -0400

    Add resume for Tyler Senecal

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,14 +1,14 @@
 "use client";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { Download, Send } from 'lucide-react';
+import { Download, Send } from "lucide-react";
 
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 const myFont = localFont({
-  src: '../fonts/kimberley.otf',
-  weight: '200',
-  style: 'normal',
-})
+  src: "../fonts/kimberley.otf",
+  weight: "200",
+  style: "normal",
+});
 
 import {
   RiBriefcase4Fill,
@@ -16,7 +16,7 @@ import {
   RiTodoFill,
   RiArrowUpSLine,
   RiArrowDownSLine,
-} from 'react-icons/ri';
+} from "react-icons/ri";
 
 // Components
 import DevImg from "./DevImg";
@@ -221,40 +221,45 @@ const projectData = [
 
 const Hero = () => {
   return (
-    <section className='py-12 xl:py-24 xxl:py-36 xxl2:py-48 md:h-[98vh] lg:h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
+    <section className="py-12 xl:py-24 xxl:py-36 xxl2:py-48 md:h-[98vh] lg:h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none">
       <div className="container mx-auto">
-        <div className='flex justify-between gap-x-8'>
+        <div className="flex justify-between gap-x-8">
           {/* Text */}
-          <div className='flex max-w-[680px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left'>
-            <div className='text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]'>
+          <div className="flex max-w-[680px] flex-col justify-center mx-auto xl:mx-0 text-center xl:text-left">
+            <div className="text-sm uppercase font-semibold mb-4 text-primary tracking-[4px]">
               Graphic Designer
             </div>
-            <h1 className={`${myFont.className} h1 mb-4 text-white antialiased font-sans`}>Hello, my name is Tyler Senecal</h1>
-            <p className='subtitle max-w-[490px] mx-auto xl:mx-0'>
-              I am a graphic designer based in Saratoga Springs,
-              New York specializing in marketing, branding, and
-              digital advertising.
+            <h1
+              className={`${myFont.className} h1 mb-4 text-white antialiased font-sans`}
+            >
+              Hello, my name is Tyler Senecal
+            </h1>
+            <p className="subtitle max-w-[490px] mx-auto xl:mx-0">
+              I am a graphic designer based in Saratoga Springs, New York
+              specializing in marketing, branding, and digital advertising.
             </p>
             {/* Buttons */}
-            <div className='flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12'>
-              <Link href='/contact'>
-                <Button className='gap-x-2 w-[225px]'>
+            <div className="flex flex-col gap-y-3 md:flex-row gap-x-3 mx-auto xl:mx-0 mb-12">
+              <Link href="/contact">
+                <Button className="gap-x-2 w-[225px]">
                   Contact Me <Send size={18} />
                 </Button>
               </Link>
-              <Button variant='secondary' className='gap-x-2 w-[225px]'>
-                Download Resume
-                <Download size={18} />
-              </Button>
+              <a href="/ResumeTJS.pdf" download="ResumeTJS">
+                <Button variant="secondary" className="gap-x-2 w-[225px]">
+                  Download Resume
+                  <Download size={18} />
+                </Button>
+              </a>
             </div>
             {/* Socials */}
             <Socials
-              containerStyles='flex gap-x-6 xxl:gap-x-10 mx-auto xl:mx-0'
-              iconsStyles='text-primary text-[27px] xxl:text-[30px] hover:text-foreground transition-all'
+              containerStyles="flex gap-x-6 xxl:gap-x-10 mx-auto xl:mx-0"
+              iconsStyles="text-primary text-[27px] xxl:text-[30px] hover:text-foreground transition-all"
             />
           </div>
           {/* Image */}
-          <div className='hidden xl:flex relative'>
+          <div className="hidden xl:flex relative">
             {/* Badge 1 */}
             {/*
             <Badge
@@ -284,7 +289,7 @@ const Hero = () => {
               badgeText='Happy Clients'
             />
             */}
-            <div className='bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2'></div>
+            <div className="bg-hero_shape2_light dark:bg-hero_shape2_dark w-[500px] h-[500px] bg-no-repeat absolute -top-1 -right-2"></div>
             {/* slider*/}
             <div className="xl:absolute xxl:max-w-[850px] xxl2:max-w-[950px] xl:max-w-[600px] xxl:-right-1 xl:right-0 xxl:-top-[4vh] xl:top-[4vh]">
               <Swiper
@@ -295,26 +300,26 @@ const Hero = () => {
                 pagination={{ clickable: true }}
               >
                 {/* show only the projects with frontPage set to true for the slides */}
-                {projectData.filter(project => project.frontPage).map((project, index) => {
-                  return (
-                    <SwiperSlide key={index}>
-                      <HeroCard project={project} />
-                    </SwiperSlide>
-                  );
-                })}
-
+                {projectData
+                  .filter((project) => project.frontPage)
+                  .map((project, index) => {
+                    return (
+                      <SwiperSlide key={index}>
+                        <HeroCard project={project} />
+                      </SwiperSlide>
+                    );
+                  })}
               </Swiper>
             </div>
           </div>
         </div>
-
       </div>
       {/* Icons */}
-      <div className='hidden md:flex absolute justify-center w-full bottom-12 animate-bounce'>
-        <RiArrowDownSLine className='text-3xl text-primary' />
+      <div className="hidden md:flex absolute justify-center w-full bottom-12 animate-bounce">
+        <RiArrowDownSLine className="text-3xl text-primary" />
       </div>
     </section>
   );
 };
 
-export default Hero
+export default Hero;
