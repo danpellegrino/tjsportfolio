608c2f1bae19a7d281b699c012b0b8f97fea77b4
Daniel Pellegrino <danpellegrino1@gmail.com>
2024-03-27 18:12:59 -0400

    Update Cta and Work components

--- components/Work.jsx
+++ components/Work.jsx
@@ -216,7 +216,7 @@ const Work = () => {
         <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
           <h2 className="section-title mb-4">Latest Projects</h2>
           <p className="subtitle mb-8">
-            Lorem, ipsum dolor sit amet consectetur adipiscing elit.
+            Check out what I've recently been working on:
           </p>
           <Link href="/projects">
             <Button className="w-[165px]">All projects</Button>
