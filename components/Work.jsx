4d340dc9fd7b6146d188d023687cd8c197aebd71
Daniel Pellegrino <danpellegrino1@gmail.com>
2024-03-28 18:10:17 -0400

    Update max-width in Work component

--- components/Work.jsx
+++ components/Work.jsx
@@ -213,7 +213,7 @@ const Work = () => {
     <section className="relative mb-12 xl:mb-48">
       <div className="container mx-auto">
         {/* text */}
-        <div className="max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
+        <div className="max-w-[300px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start">
           <h2 className="section-title mb-4">Latest Projects</h2>
           <p className="subtitle mb-8">
             Check out what I've recently been working on
