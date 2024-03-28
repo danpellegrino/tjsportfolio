1341e677338bdfb6f8fe87ce9a259f00b82e1b9b
Daniel Pellegrino <danpellegrino1@gmail.com>
2024-03-28 04:40:15 -0400

    Update nodemailer.jsx to use REACT_APP environment variables

--- utils/nodemailer.jsx
+++ utils/nodemailer.jsx
@@ -1,8 +1,8 @@
 import nodemailer from "nodemailer";
 import SMPTransport from "nodemailer-smtp-transport";
 
-export const smtpEmail = process.env.GOOGLE_EMAIL;
-export const smtpPassword = process.env.GOOGLE_PASSWORD;
+export const smtpEmail = process.env.REACT_APP_GOOGLE_EMAIL;
+export const smtpPassword = process.env.REACT_APP_GOOGLE_PASSWORD;
 
 export const transporter = nodemailer.createTransport(
   SMPTransport({
