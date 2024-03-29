31b74216187dfadf6fcbbe86ea21f1ba60be0cd9
Daniel Pellegrino <danpellegrino1@gmail.com>
2024-03-29 16:43:45 -0400

    Update contact page layout and illustration size

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -7,7 +7,7 @@ const Contact = () => {
     <section>
       <div className="container mx-auto">
         {/* text & illustration */}
-        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-6 xl:mb-24">
+        <div className="grid xl:grid-cols-2 pt-12 xl:h-[480px] mb-2">
           {/* text */}
           <div className="flex flex-col justify-center">
             <div className="flex items-center gap-x-4 text-primary text-lg mb-4">
@@ -21,29 +21,29 @@ const Contact = () => {
             </p>
           </div>
           {/* illustration */}
-          <div className="hidden xl:flex w-full bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-top bg-no-repeat" />
+          <div className="hidden xl:flex mx-auto w-[250px] bg-contact_illustration_light dark:bg-contact_illustration_dark bg-contain bg-[center_top_3rem] bg-no-repeat" />
         </div>
         {/* info text & form */}
         <div className="grid xl:grid-cols-2 mb-24 xl:mb-32">
           {/* info text */}
-          <div className="flex flex-col gap-y-4 xl:gap-y-14 mb-12 xl:mb-24 text-base xl:text-lg">
+          <div className="flex flex-col gap-y-4 xl:gap-y-12 mb-12 xl:mb-24 text-base xl:text-lg">
             {/* mail */}
-            <div className="flex items-center gap-x-8">
+            <div className="flex items-center gap-x-6">
               <MailIcon size={18} className="text-primary" />
               <div>contact@tjs-designs.com</div>
             </div>
             {/* address */}
-            <div className="flex items-center gap-x-8">
+            <div className="flex items-center gap-x-6">
               <HomeIcon size={18} className="text-primary" />
               <div>Saratoga Springs, NY</div>
             </div>
             {/* phone */}
-            <div className="flex items-center gap-x-8">
+            <div className="flex items-center gap-x-6">
               <PhoneCall size={18} className="text-primary" />
               <div>+518 299 8486</div>
             </div>
           </div>
-          <div className="relative xl:-top-10">
+          <div className="relative xl:-top-12">
             <Form />
           </div>
         </div>
